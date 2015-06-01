a53f180832756807ea1f71454418b32eaa1e03c8
CodeSuperHero <303040565@qq.com>
2015-06-02 01:37:08 +0800

    控制层

--- UnityFrame/Assets/Scripts/Core/Controller.cs
+++ UnityFrame/Assets/Scripts/Core/Controller.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Controller : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+namespace CodeSuperHero.UF
+{
+    public class Controller
+    {
+    }
 }
+
