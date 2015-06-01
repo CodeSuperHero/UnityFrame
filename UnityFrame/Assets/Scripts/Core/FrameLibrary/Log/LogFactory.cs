2f9578b96319fb990e7204c22d1714e39a0c1b53
CodeSuperHero <303040565@qq.com>
2015-06-02 02:07:55 +0800

    添加LogFactory,增加ILog接口的工厂方法 缓存同名ILog接口 用以优化同个ILog接口的反复获取

---
+++ UnityFrame/Assets/Scripts/Core/FrameLibrary/Log/LogFactory.cs
@@ -0,0 +1,35 @@
+﻿//CodeSuperHero 20150602
+
+using System.Collections.Generic;
+
+namespace CodeSuperHero.UF
+{
+    public class LogFactory
+    {
+        private static Dictionary<string, ILog> _logDic = new Dictionary<string, ILog>();
+
+        public static ILog GetLog(string logName, string logColor = "")
+        {
+            if (!DebugLog.instance.output)
+            {
+                return DebugLog.instance;
+            }
+
+            ILog retLog = null;
+
+            if (!_logDic.TryGetValue(logName, out retLog))
+            {
+                retLog = new Log(logName, logColor);
+                _logDic.Add(logName, retLog);
+            }
+
+            return retLog;
+        }
+
+        public static void Clear()
+        {
+            _logDic.Clear();
+        }
+    }
+}
+
