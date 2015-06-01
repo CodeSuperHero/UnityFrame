fe716cfc7074b200481cce90e587b514b389e28e
CodeSuperHero <303040565@qq.com>
2015-06-02 01:44:48 +0800

    日志输出接口

---
+++ UnityFrame/Assets/Scripts/Core/Interface/ILog.cs
@@ -0,0 +1,18 @@
+﻿//CodeSuperHero 20150602
+
+namespace CodeSuperHero.UF
+{
+    public interface ILog
+    {
+        bool output { get; set; }
+
+        void LogNormal(object info);
+        void LogNormal(string info, params object[] args);
+
+        void LogWarning(object info);
+        void LogWarning(string info, params object[] args);
+
+        void LogError(object info);
+        void LogError(string info, params object[] args);
+    }
+}
